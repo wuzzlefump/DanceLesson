f88dec1dc6c12c690f02f59f2acd2fba5241b3c1
wuzzlefump <paulelliottwork@gmail.com>
2020-10-13 18:07:17 -0600

    get values

---
+++ public/student.js
@@ -0,0 +1,42 @@
+
+$(document).ready(()=>{
+$("#studSearch").on("click",(e)=>{
+    e.preventDefault();
+    let search = $("#search").val();
+    console.log(search.toLowerCase())
+
+})
+
+$("#upStudent").on("click",(e)=>{
+    e.preventDefault()
+    let name = $("#upname").val().toUpperCase().trim()
+    let bday = $("#upbday").val()
+    let level = $("#uplevel").val().toUpperCase().trim()
+    let dances = $("#updances").val().split(',')
+    console.log(name)
+    console.log(bday)
+    console.log(level)
+    console.log(dances)
+})
+
+$("#delStudent").on("click",(e)=>{
+    e.preventDefault();
+
+
+})
+
+$("#crStudent").on("click",(e)=>{
+    e.preventDefault();
+    let name = $("#crname").val().toUpperCase().trim()
+    let bday = $("#crbday").val()
+    let level = $("#crlevel").val().toUpperCase().trim()
+    let dances = $("#crdances").val().split(',')
+    console.log(name)
+    console.log(bday)
+    console.log(level)
+    console.log(dances)
+
+})
+
+
+})
